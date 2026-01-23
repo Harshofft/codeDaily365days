021be73b9b4acffce26d38b10843c962162a7978
Harshofft <hdharsh122@gmail.com>
2026-01-23 22:42:08 +0530

    dsa

--- 100xdev Dsa/introClass1.cpp
+++ 100xdev Dsa/introClass1.cpp
@@ -13,10 +13,54 @@ int main(){
     //     cout << endl;
     // }
     
-    cout<<"*****"<<endl;
-    cout<<"   * "<<endl;
-    cout<<"  *  "<<endl;
-    cout<<" *   "<<endl;
-    cout<<"*****"<<endl;
-    return 0;
+    // ********creating z with cout 
+
+    // cout<<"*****"<<endl;
+    // cout<<"   * "<<endl;
+    // cout<<"  *  "<<endl;
+    // cout<<" *   "<<endl;
+    // cout<<"*****"<<endl;
+    // return 0;
+    
+    
+    //*****************table  */
+
+    // int table =5;
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     cout<<table<<" * "<<i<<" = "<<table*i<<endl;
+    // }
+    // return 0;
+
+
+    // *********area calculate*****
+
+    // int length , breath;
+    // cout<<"enter length and breath"<<endl;
+    // cin>>length;
+    // cin>>breath;
+    // int area = length*breath;
+    // int Perimeter= 2*(length+breath);
+
+    // cout<<"Area = "<<area<<endl;
+    // cout<<"Perimeter = "<<Perimeter;
+
+    //******calculator ***** */
+
+    // int a , b;
+    // cin>>a>>b;
+    // cout<<a<<" + "<<b<<" = "<<a+b<<endl;
+    // cout<<a<<" - "<<b<<" = "<<a-b<<endl;
+    // cout<<a<<" * "<<b<<" = "<<a*b<<endl;
+    // cout<<a<<" / "<<b<<" = "<<a/b<<endl;
+    // cout<<a<<" % "<<b<<" = "<<a%b<<endl;
+    // return 0;
+
+    // add the 2 last digit of 2 no
+
+    int a ,b ;
+    cin>>a>>b;
+    a=a%10;
+    b=b%10;
+    cout<<a+b;
 }
